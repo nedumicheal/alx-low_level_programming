6e81c9c3b35afe0230c7f408119774479c153b52
nedumicheal <crystallmike@gmail.com>
2023-06-20 14:47:23 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *prints the alphabets ten times, in lowercase,
+ * print_alphabet_x10 -prints the alphabets ten times, in lowercase,
  * followed by a new line
  */
 void print_alphabet_x10(void)
