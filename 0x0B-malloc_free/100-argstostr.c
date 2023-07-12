1d05429aeecac90c4fed212a187a15efaa067a3e
nedu micheal <crystallmike@gmail.com>
2023-07-12 05:45:47 -0700

    malloc fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,41 +1,39 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * argstostr - main entry
  * @ac: int input
  * @av: double pointer array
  * Return: 0
  */
-
 char *argstostr(int ac, char **av)
 {
-	int a, b, c = 0, d = 0;
+	int d, e, f = 0, g = 0;
 	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (a = 0; a < ac; a++)
+	for (d = 0; d < ac; d++)
 	{
-		for (b = 0; av[a][b]; b++)
-			d++;
+		for (e = 0; av[d][e]; e++)
+			g++;
 	}
-	d += ac;
+	g += ac;
 
-	str = malloc(sizeof(char) * d + 1);
+	str = malloc(sizeof(char) * g + 1);
 	if (str == NULL)
 		return (NULL);
-	for (a = 0; a < ac; a++)
+	for (d = 0; d < ac; d++)
 	{
-	for (b = 0; av[a][b]; b++)
+	for (e = 0; av[d][e]; e++)
 	{
-		str[c] = av[a][b];
-		c++;
+		str[f] = av[d][e];
+		f++;
 	}
-	if (str[c] == '\0')
+	if (str[f] == '\0')
 	{
-		str[c++] = '\b';
+		str[f++] = '\e';
 	}
 	}
 	return (str);
