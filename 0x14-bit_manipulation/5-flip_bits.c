b506a464a2124df54fba5f807e340015c829cb6c
nedu micheal <crystallmike@gmail.com>
2023-08-03 15:27:24 -0700

    bit manipulation task

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,6 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	int dx, tom = 0;
 	unsigned long int ben;
 	unsigned long int tim = n ^ m;
+
 	for (dx = 63; dx >= 0; dx--)
 	{
 		ben = tim >> dx;
